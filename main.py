1fcc0e229697de65cc65065c24302892d70ec251
Sanmi01 <supersanmi01@gmail.com>
2022-05-28 10:57:42 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+    if type(word) != str or type(anagram) != str:
+        return "Not a string"
+    else:
+        return(sorted(word.replace(" ", "").lower())==sorted(anagram.replace(" ", "").lower()))
+
+print(find_anagram("h e y br o t h e r", "hey bother r")) # --> True
+print(find_anagram("mite", "tiMe")) # --> True
+print(find_anagram("cheap", "peach")) # --> True
+print(find_anagram("Hello", "Dinner")) # --> False
+print(find_anagram(12, 21)) # --> Not a string
+print(find_anagram(['a', 'b'], ['a','b'])) # --> Not a string
+
